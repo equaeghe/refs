f7727839b1576581e60a9ba81fed10010b9108c8
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2013-05-20 16:15:26 +0200

    update script

--- blx2bib.sh
+++ blx2bib.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-biber -O refs_resolved.bib --tool_resolve --tool refs.bib
-biber -O refs_bibtex.bib -g blx2bib.conf --tool refs_resolved.bib
+biber -O refs_pre.bib -g blx2bib-pre.conf --tool refs.bib
+biber -O refs_resolved.bib --tool_resolve --tool refs_pre.bib
+biber -O refs_bibtex.bib -g blx2bib-post.conf --tool refs_resolved.bib
