a5591a6cd1f068180d122da7d8a43fd2eb35c908
Erik Quaeghebeur <Erik.Quaeghebeur@UGent.be>
2013-05-23 13:18:29 +0200

    convert int months to BibTeX macros for BibTeX format

--- blx2bib.sh
+++ blx2bib.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 biber -O refs_pre.bib -g blx2bib-pre.conf --tool refs.bib
 biber -O refs_resolved.bib --tool_resolve --tool refs_pre.bib
-biber -O refs_bibtex.bib -g blx2bib-post.conf --tool refs_resolved.bib
+biber -O refs_bibtex.bib -g blx2bib-post.conf --tool_macro_fields=MONTH --tool refs_resolved.bib
